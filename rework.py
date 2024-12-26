e2a456677690e3f26a0362a4ba4034fcf84d49c3
thmaz <thomasplmn@gmail.com>
2024-12-26 11:22:36 +0100

    latest

--- rework.py
+++ rework.py
@@ -15,5 +15,124 @@ races_df = pd.read_sql_query('SELECT * FROM race_results', conn)
 # df = races_df.set_index('rider_id').join(riders_df.set_index('rider_id'))
 
 conn.close()
-print(riders_df.describe)
+
+
+
+"""
+Convert the 'Date' column to DateTime format
+Regarding races_df dataframe!
+"""
+
+races_df['Date'] = pd.to_datetime(races_df['Date'], errors='coerce', format='%d %B %Y')
+
+# Remove rows where 'Date' is NaT
+races_df = races_df.dropna(subset=['Date'])
+
+# Normalize 'Date' to strip out time if it's present (this keeps just the date part)
+races_df['Date'] = races_df['Date'].dt.normalize()
+
+# Extract the month and year from the column and put them in their own columns
+races_df['Month'] = races_df['Date'].dt.month
+races_df['Year'] = races_df['Date'].dt.year
+
+print("Unique values for 'months': ", races_df['Month'].unique(), "\n")
+print("Unique values for 'years': ",races_df['Year'].unique())
+
+
+
+"""
+Converting the timetable to total seconds
+"""
+def time_to_seconds(time_str):
+    # Remove commas and any spaces
+    time_str = time_str.replace(',', '').strip()
+
+    # Check the consistency of the time format using regular expressions
+    match = re.match(r'(\d{1,2}):(\d{2}):(\d{2})', time_str)
+    if match:
+        hours, minutes, seconds = map(int, match.groups())
+        total_seconds = hours * 3600 + minutes * 60 + seconds
+        if total_seconds == 0:
+            return np.nan
+        return total_seconds
+
+    # Do the same as the loop above, but now for MM:SS format.
+    match = re.match(r'(\d{1,2}):(\d{2})', time_str)
+    if match:
+        minutes, seconds = map(int, match.groups())
+        total_seconds = minutes * 60 + seconds
+        if total_seconds == 0:
+            return np.nan  
+        return total_seconds
+
+    # Do the same but for 0:00, 0:01, etc.
+    match = re.match(r'(\d{1,2}):(\d{1,2})', time_str)
+    if match:
+        minutes, seconds = map(int, match.groups())
+        total_seconds = minutes * 60 + seconds
+        if total_seconds == 0:
+            return np.nan 
+        return total_seconds
+    
+    # If format doesn't match, return NaN
+    return np.nan
+
+# Apply the conversion function to the 'Time' column
+races_df['Time_seconds'] = races_df['Time'].apply(time_to_seconds)
+
+print(races_df[['Time', 'Time_seconds']].head(10))
+races_df.info()
+
+
+
+"""
+Converting timelag to total seconds
+"""
+def timelag_to_seconds(timelag_str):
+    # Check if the timelag_str contains missing values
+    if pd.isna(timelag_str):
+        return np.nan
+
+    timelag_str = timelag_str.lstrip('+').strip()
+
+    match = re.match(r'(\d{1,2}):(\d{2}):(\d{2})', timelag_str)
+    if match:
+        hours, minutes, seconds = map(int, match.groups())
+        return hours * 3600 + minutes * 60 + seconds
+
+    match = re.match(r'(\d{1,2}):(\d{2})', timelag_str)
+    if match:
+        minutes, seconds = map(int, match.groups())
+        return minutes * 60 + seconds
+    
+    match = re.match(r'(\d+):(\d+)', timelag_str)
+    if match:
+        minutes, seconds = map(int, match.groups())
+        return minutes * 60 + seconds
+
+    return np.nan
+
+races_df['Timelag_seconds'] = races_df['Timelag'].apply(timelag_to_seconds)
+
+races_df['Timelag_seconds'] = races_df['Timelag_seconds'].replace(0.0, np.nan)
+
+print(races_df[['Timelag', 'Timelag_seconds']].head(10))
+
+
+
+"""
+Converting distance into single numerical value
+This means stripping 'km' from string and converting the remaining values into float64
+"""
+races_df['Length'] = races_df['Length'].str.replace(' km', '', regex=False)
+
+# Convert to numeric and replace 0 with NaN
+races_df['Length'] = pd.to_numeric(races_df['Length'], errors='coerce')  # Convert to numeric and handle errors
+
+# Replace 0 values with NaN
+races_df['Length'] = races_df['Length'].replace(0.0, np.nan)
+
+print(races_df['Length'])
+
+
 print("done")
