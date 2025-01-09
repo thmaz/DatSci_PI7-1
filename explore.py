377a878f8aa51e4555daee46eda2c328a1ecf1cc
thmaz <thomasplmn@gmail.com>
2025-01-09 19:18:03 +0100

    7.2-1 done

--- explore.py
+++ explore.py
@@ -17,4 +17,16 @@ races_df = pd.read_sql_query('SELECT * FROM race_results', conn)
 
 conn.close()
 
-riders_df['rider_id'].isna.sum()
+print("Null values riders_df:\n", riders_df.isna().sum(), "\n")
+print("Null values races_df:\n", races_df.isna().sum(), "\n")
+
+races_dfnew = races_df.dropna(subset=['rider_id'])
+
+print("Null values before dropping:\n", races_df['rider_id'].isna().sum(), "\n")
+print("Null values after dropping:\n", races_dfnew['rider_id'].isna().sum(), "\n")
+
+df = races_df.set_index('rider_id').join(riders_df.set_index('rider_id'))
+
+df = df.reset_index()
+
+print("Total after dropping: \n", df.isna().sum())
--- rework.py
+++ rework.py
@@ -1,12 +1,8 @@
 import sqlite3 as lite
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import re
 import numpy as np
 import ast
-
-# from sklearn.impute import KNNImputer
 
 conn = lite.connect('cycling_big.db')
 
@@ -16,8 +12,6 @@ races_df = pd.read_sql_query('SELECT * FROM race_results', conn)
 # df = races_df.set_index('rider_id').join(riders_df.set_index('rider_id'))
 
 conn.close()
-
-
 
 """
 Convert the 'Date' column to DateTime format
@@ -39,11 +33,10 @@ races_df['Year'] = races_df['Date'].dt.year
 print("Unique values for 'months': ", races_df['Month'].unique(), "\n")
 print("Unique values for 'years': ",races_df['Year'].unique())
 
-
-
 """
 Converting the timetable to total seconds
 """
+
 def time_to_seconds(time_str):
     # Remove commas and any spaces
     time_str = time_str.replace(',', '').strip()
@@ -81,12 +74,13 @@ def time_to_seconds(time_str):
 # Apply the conversion function to the 'Time' column
 races_df['Time_seconds'] = races_df['Time'].apply(time_to_seconds)
 
-print(races_df[['Time', 'Time_seconds']].head(10))
-races_df.info()
+# print(races_df[['Time', 'Time_seconds']].head(10))
+# races_df.info()
 
 """
 Converting timelag to total seconds
 """
+
 def timelag_to_seconds(timelag_str):
     # Check if the timelag_str contains missing values
     if pd.isna(timelag_str):
@@ -117,8 +111,6 @@ races_df['Timelag_seconds'] = races_df['Timelag_seconds'].replace(0.0, np.nan)
 
 print(races_df[['Timelag', 'Timelag_seconds']].head(10))
 
-
-
 """
 Converting distance into single numerical value
 This means stripping 'km' from string and converting the remaining values into float64
@@ -135,7 +127,103 @@ races_df['Length'] = races_df['Length'].replace(0.0, np.nan)
 # print(races_df['Length'])
 
 """
-Imputating data
+Splitting values from 'rdr' and putting the split values into separate columns
 """
 
-print("done")
+# Function to convert the 'rdr' string to separate ranking columns
+def extract_rankings(rdr_str):
+    try:
+        # Converting str to dict
+        rankings = ast.literal_eval(rdr_str)
+        
+        # Extracting ranks, while also handling missing keys
+        pcs_rnk = rankings.get('PCS Ranking', np.nan)
+        uci_rnk = rankings.get('UCI World Ranking', np.nan)
+        alltime_rnk = rankings.get('Specials | All Time Ranking', np.nan)
+        
+        return pd.Series([pcs_rnk, uci_rnk, alltime_rnk])
+    except:
+        return pd.Series([np.nan, np.nan, np.nan])
+
+# Apply the function to the 'rdr' column
+riders_df[['PCS_Rnk', 'UCI_Rnk', 'AllTime_Rnk']] = riders_df['rdr'].apply(extract_rankings)
+
+# Entries should be turned into numeric values, where errors get turned into NaN
+riders_df['PCS_Rnk'] = pd.to_numeric(riders_df['PCS_Rnk'], errors='coerce')
+riders_df['UCI_Rnk'] = pd.to_numeric(riders_df['UCI_Rnk'], errors='coerce')
+riders_df['AllTime_Rnk'] = pd.to_numeric(riders_df['AllTime_Rnk'], errors='coerce')
+
+# print(riders_df[['fullname', 'PCS_Rnk', 'UCI_Rnk', 'AllTime_Rnk']])
+
+"""
+Convert stage types to binary with label encoding
+"""
+
+races_df['Stage_Type_bin'] = races_df['Stage_Type'].map({'RR': 0, 'ITT': 1})
+
+"""
+Splitting values from 'pps' and putting those values into separate columns.
+"""
+
+def extract_points(pps_str):
+    try:
+        points = ast.literal_eval(pps_str)
+        
+        day_pnt = np.nan if points.get('One day races', '0') == '0' else points.get('One day races', np.nan)
+        gc_pnt = np.nan if points.get('GC', '0') == '0' else points.get('GC', np.nan)
+        tt_pnt = np.nan if points.get('Time trial', '0') == '0' else points.get('Time trial', np.nan)
+        sprint_pnt = np.nan if points.get('Sprint', '0') == '0' else points.get('Sprint', np.nan)
+        climb_pnt = np.nan if points.get('Climber', '0') == '0' else points.get('Climber', np.nan)
+        
+        return pd.Series([day_pnt, gc_pnt, tt_pnt, sprint_pnt, climb_pnt])
+    except:
+        return pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan])
+
+riders_df[['Day_Pnt', 'GC_Pnt', 'TT_Pnt', 'Sprint_Pnt', 'Climb_Pnt']] = riders_df['pps'].apply(extract_points)
+
+riders_df['Day_Pnt'] = pd.to_numeric(riders_df['Day_Pnt'], errors='coerce')
+riders_df['GC_Pnt'] = pd.to_numeric(riders_df['GC_Pnt'], errors='coerce')
+riders_df['TT_Pnt'] = pd.to_numeric(riders_df['TT_Pnt'], errors='coerce')
+riders_df['Sprint_Pnt'] = pd.to_numeric(riders_df['Sprint_Pnt'], errors='coerce')
+riders_df['Climb_Pnt'] = pd.to_numeric(riders_df['Climb_Pnt'], errors='coerce')
+
+# print(riders_df[['fullname', 'Day_Pnt', 'GC_Pnt', 'TT_Pnt', 'Sprint_Pnt', 'Climb_Pnt']])
+
+
+"""
+Join the rider and race tables together, using the rider_id as an index
+"""
+
+df = races_df.set_index('rider_id').join(riders_df.set_index('rider_id'))
+
+"""
+Dropping columns that are not needed for analysis
+"""
+
+# Note: fix the long list
+df.drop(['Time', 'Timelag', 'rdr', 'pps', 'birthdate', 'rider_url', 'Race_url', 'Stage_url', 'Circuit', 'Race_Name', 'Stage_Name', 'Start', 'Finish', 'Category', 'Stage_Type'], axis=1, inplace=True)
+
+# Replace all remaining zero values with NaN
+df = df.replace(0, np.nan)
+
+"""
+Imputating data over values that have a normal distribution
+"""
+
+df_imp = df.copy()
+
+#numeric_columns = df_imp.select_dtypes(include=['float64', 'int64']).columns
+
+# We want certain columns, like rankings, excluded from mean imputation
+#columns_excl = ['id', 'Rnk', 'PCS_Rnk', 'UCI_Rnk', 'AllTime_Rnk', 'Stage_Type_bin', 'Race_ID', 'Time_seconds', 'Timelag_seconds', 'Stage_Number']
+
+# Remove columns to exclude from the list of numeric columns
+columns_impute = ['Age', 'Length', 'height', 'weight']
+
+# Perform mean imputation only on the selected columns
+df_imp[columns_impute] = df_imp[columns_impute].fillna(df_imp[columns_impute].mean())
+
+print(columns_impute)
+
+print(df.isnull().sum())
+print("\ndone")
